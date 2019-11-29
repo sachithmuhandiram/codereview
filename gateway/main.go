8ea954de0e87769e779dd8f638a3c834dca52b1f
Sachith Muhandiram <sachith@vizuamatix.com>
2019-11-29 06:30:58 +0530

    Generating UUID success

--- gateway/main.go
+++ gateway/main.go
@@ -1,44 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
 	"regexp"
-	"strings"
 
 	"github.com/google/uuid"
-	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/ext"
-	"github.com/uber/jaeger-client-go"
-	"github.com/uber/jaeger-client-go/config"
+	logs "github.com/sirupsen/logrus"
 )
-
-// For generateUUID function return
-type UUID [16]byte
-
-type apiTracer struct {
-	tracer opentracing.Tracer
-}
-
-// Tracing function
-func startTracing(service string) opentracing.Tracer {
-	cfg := &config.Configuration{
-		Sampler: &config.SamplerConfig{
-			Type:  "const",
-			Param: 1,
-		},
-		Reporter: &config.ReporterConfig{
-			LogSpans: true,
-		},
-	}
-	tracer, _, err := cfg.New(service, config.Logger(jaeger.StdLogger))
-	if err != nil {
-		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
-	}
-	return tracer
-}
 
 // This is the main module, this should update into an API gateway.
 // Initial step, just routing functionality will be used.
@@ -46,26 +16,29 @@ func startTracing(service string) opentracing.Tracer {
 
 func main() {
 
-	log.Println("API gateway started at port : 7070")
-	apitracer := apiTracer{startTracing("Validate Email")}
+	logs.WithFields(logs.Fields{
+		"package":  "API-Gateway",
+		"function": "main",
+	}).Info("API - Gateway started at 7070")
 
-	http.HandleFunc("/getemail", apitracer.validatemail)
-	http.HandleFunc("/response", getRespose)
+	apiUuid := generateUUID()
+
+	log.Println("UUID generated : ", apiUuid)
+
+	http.HandleFunc("/getemail", validatemail)
 
 	http.ListenAndServe(":7070", nil)
 }
 
-// This will validate email address has valid syntax (apitracer apiTracer)
-func (api *apiTracer) validatemail(res http.ResponseWriter, req *http.Request) {
-
-	validateEmailSpan := api.tracer.StartSpan("Email address checking")
-
-	// Get UUID
-	uuid := getUUID()
+// This will validate email address has valid syntax
+func validatemail(res http.ResponseWriter, req *http.Request) {
 
 	// Check method
 	if req.Method != "POST" {
-		log.Panic("Email form data is not Post")
+		logs.WithFields(logs.Fields{
+			"package":  "API - Gateway",
+			"function": "validatemail",
+		}).Error("Request method is not POST")
 		//http.Redirect(res, req, "/", http.StatusSeeOther) // redirect back to register
 	}
 
@@ -74,69 +47,54 @@ func (api *apiTracer) validatemail(res http.ResponseWriter, req *http.Request) {
 	validEmail := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`) // regex to validate email address
 
 	if validEmail.MatchString(email) {
-		log.Println("Valid email address format received")
-		log.Println("Email is passed to user module to validate ")
-		httpClient := http.Client{}
+		logs.WithFields(logs.Fields{
+			"package":  "API-Gateway",
+			"function": "validatemail",
+		}).Info("Valida email format received")
 
-		form := url.Values{}
-		form.Set("uuid", uuid)
-		form.Set("email", email)
+		logs.WithFields(logs.Fields{
+			"package":  "API-Gateway",
+			"function": "validatemail",
+			"email":    email,
+		}).Info("Email will pass to User - Service")
 
-		//b := bytes.NewBufferString(form.Encode())
-		userserviceUrl := "http://user:7071/checkemail"
-
-		checkEmailReq, err := http.NewRequest("POST", userserviceUrl, strings.NewReader(form.Encode()))
-		if err != nil {
-			log.Println(err)
-		}
-
-		ext.SpanKindRPCClient.Set(validateEmailSpan)
-		ext.HTTPUrl.Set(validateEmailSpan, userserviceUrl)
-		ext.HTTPMethod.Set(validateEmailSpan, "POST")
-
-		validateEmailSpan.Tracer().Inject(
-			validateEmailSpan.Context(),
-			opentracing.HTTPHeaders,
-			opentracing.HTTPHeadersCarrier(req.Header),
-		)
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-
-		resp, err := httpClient.Do(checkEmailReq)
-		//_, err = http.PostForm("http://user:7071/checkemail", url.Values{"uuid": {uuid}, "email": {email}})
+		_, err := http.PostForm("http://user:7071/checkemail", url.Values{"email": {email}})
 
 		if err != nil {
-			log.Println("Couldnt verify email address user service sends an error : ", err)
+			logs.WithFields(logs.Fields{
+				"package":  "API-Gateway",
+				"function": "validatemail",
+				"email":    email,
+				"error":    err,
+			}).Error("Error posting data to User - Service")
 		}
-		defer resp.Body.Close()
 
 	} else {
-		log.Println("Wrong email address format")
+		logs.WithFields(logs.Fields{
+			"package":  "API-Gateway",
+			"function": "validatemail",
+			"email":    email,
+		}).Error("Wrong email format received")
 		// Return to register window
 		//return false
 	}
-
-	//defer gatewayTracerClose.Close()
-	defer validateEmailSpan.Finish()
 }
 
-func getUUID() string {
+func generateUUID() string {
 	// Generating UUID
 	uuid, err := uuid.NewUUID()
 
 	if err != nil {
-		log.Println("Couldnt generate a UUID for the request")
+		if err != nil {
+			logs.WithFields(logs.Fields{
+				"package":  "API-Gateway",
+				"function": "generateUUID",
+				"error":    err,
+			}).Error("Couldnt generate a UUID")
+		}
 
 		// Return to error page
 		//http.Redirect(loginResponse, loginRequest, "/", http.StatusSeeOther)
 	}
 	return uuid.String()
-}
-
-func getRespose(res http.ResponseWriter, req *http.Request) {
-
-	uuid := req.FormValue("uuid")
-	serviceID := req.FormValue("serviceID")
-	resultCode := req.FormValue("resultCode")
-
-	log.Println("UUID,ServiceID,resultCode", uuid, serviceID, resultCode)
 }
