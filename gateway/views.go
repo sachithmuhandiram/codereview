2d2d9df0ed896627d19254a24f02829bff5b0924
sachithmuhandiram <sachithnalaka@gmail.com>
2020-06-07 16:20:47 +0530

    feat: added view to /codesubmit, need to populate dropdown

--- gateway/views.go
+++ gateway/views.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	"net/http"
+	"html/template"
 )
 func registerView(res http.ResponseWriter,req *http.Request){
 	http.ServeFile(res, req,"views/register.html")
@@ -16,5 +17,21 @@ func getEmailView(res http.ResponseWriter,req *http.Request){
 }
 
 func codeSubmitView(res http.ResponseWriter, req *http.Request){
-	http.ServeFile(res, req,"views/codesubmit.html")
+
+	t, err := template.ParseFiles("views/codesubmit.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	colours := map[string]string{
+		"1": "blue",
+		"2": "red",
+		"3": "yellow",
+	}
+
+	err = t.Execute(res, colours)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
